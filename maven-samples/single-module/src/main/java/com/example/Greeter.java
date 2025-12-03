b19cf412fb0072839c379593015f265f265c0f18
Christopherdsouza02 <127783358+Christopherdsouza02@users.noreply.github.com>
2025-12-03 16:04:08 +0530

    Simplify greeting message in Greeter.java

--- maven-samples/single-module/src/main/java/com/example/Greeter.java
+++ maven-samples/single-module/src/main/java/com/example/Greeter.java
@@ -21,7 +21,7 @@ public class Greeter {
   
   public static void main(String args[]){
     
-	  System.out.println("Hello students Good morningWelcome to the session. !!!");
+	  System.out.println("Hello students Good morning !!!");
   }
 	
 }
